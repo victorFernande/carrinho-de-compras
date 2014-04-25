43811e9dfd00d606137a70b83b8564ce96d9678f
aholawlier@gmail.com <rm68360@L202MICRO33.fiap.com.br>
2014-04-25 11:24:18 -0300

    Funções de total de itens e total de preço.

--- src/br/calebe/exemplos/ex01/Carrinho.java
+++ src/br/calebe/exemplos/ex01/Carrinho.java
@@ -11,6 +11,20 @@ public class Carrinho {
 		itens = new HashMap<Produto,Integer>();	
 	}
 
+	public int totalItens() {
+		int sum = 0;
+		for(Map.Entry<Produto,Integer> entry : itens.entrySet())
+			sum+=entry.getValue();
+		return sum;
+	}
+	
+	public double total() {
+		double sum=0;
+		for(Map.Entry<Produto,Integer> entry : itens.entrySet())
+			sum+=entry.getKey().getPreco()*entry.getValue();
+		return sum;
+	}
+	
 	public void add(Produto produto) {
 		if(itens.containsKey(produto))
 			itens.put(produto,itens.get(produto)+1);
