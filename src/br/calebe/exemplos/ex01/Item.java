4e30a4dac21507fdaedcf14c3caa8395f5b32769
aholawlier@gmail.com <rm68360@L202MICRO33.fiap.com.br>
2014-04-25 10:05:30 -0300

    Identação arrumada.

--- src/br/calebe/exemplos/ex01/Item.java
+++ src/br/calebe/exemplos/ex01/Item.java
@@ -1,22 +1,21 @@
 package br.calebe.exemplos.ex01;
+
 public class Item {
-    
-    private Produto produto;
-    private int quantidade;
 
-    public Item(Produto produto, int quantidade) {
-        this.produto = produto;
-        this.quantidade = quantidade;
-    }
-    
-    
-    @Override
-    public boolean equals(Object obj) {
-        return equals((Item) obj);
-    }
+	private Produto produto;
+	private int quantidade;
 
-    
-    public boolean equals(Item obj) {
-        return produto.equals(obj.produto);
-    }
+	public Item(Produto produto, int quantidade) {
+		this.produto = produto;
+		this.quantidade = quantidade;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return equals((Item) obj);
+	}
+
+	public boolean equals(Item obj) {
+		return produto.equals(obj.produto);
+	}
 }
--- src/br/calebe/exemplos/ex01/Produto.java
+++ src/br/calebe/exemplos/ex01/Produto.java
@@ -2,26 +2,26 @@ package br.calebe.exemplos.ex01;
 
 public class Produto {
 
-    private String nome;
-    private double preco;
-    private String genero;
+	private String nome;
+	private double preco;
+	private String genero;
 
-    public Produto(String nome, double preco) {
-        this.nome = nome;
-        this.preco = preco;
-        this.genero = genero;
-    }
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+		this.genero = genero;
+	}
 
-    public double getPreco() {
-        return preco;
-    }
+	public double getPreco() {
+		return preco;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        return equals((Produto) obj);
-    }
+	@Override
+	public boolean equals(Object obj) {
+		return equals((Produto) obj);
+	}
 
-    public boolean equals(Produto obj) {
-        return nome.equals(obj.nome);
-    }
+	public boolean equals(Produto obj) {
+		return nome.equals(obj.nome);
+	}
 }
