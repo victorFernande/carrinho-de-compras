4244ae50338bc351a215e4bab38c8532de6716e1
Screwball <aholawlier@gmail.com>
2014-05-09 00:43:48 -0300

    Adicionado serialUID.

--- src/br/calebe/exemplos/ex01/ProdutoQuantidadeException.java
+++ src/br/calebe/exemplos/ex01/ProdutoQuantidadeException.java
@@ -2,6 +2,6 @@ package br.calebe.exemplos.ex01;
 
 public class ProdutoQuantidadeException extends Exception {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 6353873866171708713L;
 
 }
