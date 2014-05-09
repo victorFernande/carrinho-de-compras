69c120dbf59f35c94f3a683e5ca53f10dc622632
Screwball <aholawlier@gmail.com>
2014-05-09 00:43:14 -0300

    Adicionado serialUID.

--- src/br/calebe/exemplos/ex01/ProdutoInexistenteException.java
+++ src/br/calebe/exemplos/ex01/ProdutoInexistenteException.java
@@ -2,5 +2,5 @@ package br.calebe.exemplos.ex01;
 
 public class ProdutoInexistenteException extends Exception {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -1952003566869360436L;
 }
