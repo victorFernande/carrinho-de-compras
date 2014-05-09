aa0f9c7b9a4dad9db97ac5ccc31cafc3f4caa744
Screwball <aholawlier@gmail.com>
2014-05-09 00:42:07 -0300

    Adicionado serialUID.

--- src/br/calebe/exemplos/ex01/CarrinhoVazioException.java
+++ src/br/calebe/exemplos/ex01/CarrinhoVazioException.java
@@ -2,5 +2,5 @@ package br.calebe.exemplos.ex01;
 
 public class CarrinhoVazioException extends Exception {
 
-	private static final long serialVersionUID = 2034287738018421461L;
+	private static final long serialVersionUID = -4507456339184782412L;
 }
