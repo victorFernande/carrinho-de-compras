59ed71caabc88b460a7deb2156f1e8a61a0c5d4b
ricardo.valle <ricardo.valle@SUPORTE02.fiap.com.br>
2014-05-13 19:04:28 -0300

    Remocação do atributo de gênero. Faremos a abstração por herança.

--- src/br/calebe/exemplos/ex01/Produto.java
+++ src/br/calebe/exemplos/ex01/Produto.java
@@ -1,15 +1,13 @@
 package br.calebe.exemplos.ex01;
 
-public class Produto {
+public abstract class Produto {
 
 	private String nome;
 	private double preco;
-	private Genero genero;
 
 	public Produto(String nome, double preco, Genero genero) {
 		this.nome = nome.toUpperCase();
 		this.preco = preco;
-		this.genero = genero;
 	}
 
 	public double getPreco() {
@@ -17,14 +15,6 @@ public class Produto {
 	}
 
 	public int hashCode() {
-		return nome.hashCode() + Long.valueOf(Double.doubleToLongBits(preco)).hashCode() + genero.hashCode();
-	}
-
-	public boolean equals(Object obj) {
-		return equals((Produto) obj);
-	}
-
-	public boolean equals(Produto obj) {
-		return nome.equals(obj.nome) && preco == obj.preco && genero.equals(obj.genero);
+		return nome.hashCode() + Long.valueOf(Double.doubleToLongBits(preco)).hashCode();
 	}
 }
